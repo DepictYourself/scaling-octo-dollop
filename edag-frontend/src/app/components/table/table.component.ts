e06e87fb0a912f8f0756db88f58bbf139e638842
DepictYourself <totha014@gmail.com>
2023-03-17 12:48:57 +0100

    display users in table component

--- edag-frontend/src/app/components/table/table.component.ts
+++ edag-frontend/src/app/components/table/table.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
-
+export class TableComponent implements OnInit{
+    
     @Input()
     name: string = "";
-
+    
     @Input()
     users: {name: string, birthday:string}[]= [];
+    
+    
+    ngOnInit(): void {
+        console.log(this.users);
+        
+    }
 
 }
