97064ec062c9d8f065af98c5974c063f2ab636a0
DepictYourself <totha014@gmail.com>
2023-03-17 11:55:56 +0100

    pass data to table components

--- edag-frontend/src/app/components/table/table.component.ts
+++ edag-frontend/src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+
+    @Input()
+    name: string = "";
+
+    @Input()
+    users: {name: string, birthday:string}[]= [];
 
 }
