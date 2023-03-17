cc609f3c8405703f4dc08fdc2dd49397c3edff65
DepictYourself <totha014@gmail.com>
2023-03-17 15:50:50 +0100

    implement delete user in department service

--- edag-frontend/src/app/services/department.service.ts
+++ edag-frontend/src/app/services/department.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, from } from 'rxjs';
 
 import { iDepartment } from '../models/iDepartment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,23 @@ export class DepartmentService {
             .then(res => res.json())
             .then(data => data)
         ); 
+    }
+
+    removeUser(department: iDepartment, deletableUser: User): Observable<iDepartment> {
+        const url = `${this.apiEndpointUrl}/${department.id}`
+        const newUsersObj = {
+            users: department.users.filter(u => u.id !== deletableUser.id)
+        }
+
+        return from(
+            fetch(url, {
+                method: "PATCH",
+                body: JSON.stringify(newUsersObj),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(res => res.json())
+            .then(json => json)
+        );
     }
 }
