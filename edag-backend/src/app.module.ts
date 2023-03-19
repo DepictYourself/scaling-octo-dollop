9739d6265b8a8fe9d91db386954a07f11ec0c235
DepictYourself <totha014@gmail.com>
2023-03-19 15:15:14 +0100

    config schema in app.module

--- edag-backend/src/app.module.ts
+++ edag-backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DepartmentSchema } from './schemas/department.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://localhost:27017/departments"),
+    MongooseModule.forFeature([{name: "Department", schema: DepartmentSchema}])
   ],
   controllers: [AppController],
   providers: [AppService],
