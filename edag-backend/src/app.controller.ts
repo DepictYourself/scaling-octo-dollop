40ded8ea867ef670facb55cc84ecddc9745ac0f6
DepictYourself <totha014@gmail.com>
2023-03-19 15:16:28 +0100

    refactor controller to use app service

--- edag-backend/src/app.controller.ts
+++ edag-backend/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EdagDto } from './DTO/edag.dto';
+import { Department } from './schemas/department.schema';
 
 @Controller()
 export class AppController {
@@ -15,10 +16,8 @@ export class AppController {
 
 
     @Post()
-    create(@Body() jsonData: EdagDto): string {
-        console.table(jsonData.departments);
-        console.table(jsonData.departments[0])
-        return `${jsonData.departments}`;
+    create(@Body() jsonData: EdagDto): Promise<Department[]> {
+        return this.appService.create(jsonData);
     }
 
 
