852965e22683a93d9a3cb533d20ebe5243ab009e
DepictYourself <totha014@gmail.com>
2023-03-19 12:11:01 +0100

    Add functions to app controller
    the findAll functionally will handle the GET and create will handle the POST
    request.

--- edag-backend/src/app.controller.ts
+++ edag-backend/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EdagDto } from './DTO/edag.dto';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) { }
 
+
+
     @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    findAll(): string {
+        return "finding all..";
     }
+
+
+    @Post()
+    create(@Body() jsonData: EdagDto): string {
+        console.table(jsonData.departments);
+        console.table(jsonData.departments[0])
+        return `${jsonData.departments}`;
+    }
+
+
 }
