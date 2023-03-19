26c47605e24b920e1f72415ec1e5701e4442b386
DepictYourself <totha014@gmail.com>
2023-03-19 22:59:45 +0100

    change mongoose config to connect to edag db

--- edag-backend/src/app.module.ts
+++ edag-backend/src/app.module.ts
@@ -5,11 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DepartmentSchema } from './schemas/department.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/departments"),
-    MongooseModule.forFeature([{name: "Department", schema: DepartmentSchema}])
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot("mongodb://localhost:27017/edag"),
+        MongooseModule.forFeature([{ name: "Department", schema: DepartmentSchema }])
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
