a9bac5abec37a59e7c986cf5639c52a989a4c8db
DepictYourself <totha014@gmail.com>
2023-03-20 10:53:20 +0100

    Change department service to connect to backend

--- edag-frontend/src/app/services/department.service.ts
+++ edag-frontend/src/app/services/department.service.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user';
 })
 export class DepartmentService {
 
-    apiEndpointUrl: string = "http://localhost:3000/departments";
+    apiEndpointUrl: string = "http://localhost:3000/";
 
     constructor() { }
 
@@ -21,52 +21,18 @@ export class DepartmentService {
         ); 
     }
 
-    addUser(department: iDepartment, creatableUser: {name: string, birthday: string}): Observable<iDepartment> {
-        const url = `${this.apiEndpointUrl}/${department.id}`
-
-        const newUser: User = {
-            id: this.getNewUserId(department),
-            name: creatableUser.name,
-            birthday: creatableUser.birthday
-        }
-        const newUserArray = department.users.concat(newUser);
-
-        return from(
-            fetch(url, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    users: newUserArray
-                })
-            })
-            .then(res => res.json())
-            .then(department => department)
-        );
-    }
-
-    private getNewUserId(department: iDepartment): number{
-        const userArrayLength = department.users.length;
-        const lastUser = department.users[userArrayLength - 1];
-        return lastUser.id + 1;
-    }
-
-    removeUser(department: iDepartment, deletableUser: User): Observable<iDepartment> {
-        const url = `${this.apiEndpointUrl}/${department.id}`
-        const newUsersObj = {
-            users: department.users.filter(u => u.id !== deletableUser.id)
-        }
-
-        return from(
-            fetch(url, {
-                method: "PATCH",
-                body: JSON.stringify(newUsersObj),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(res => res.json())
-            .then(json => json)
-        );
-    }
+    // postDepartments(creatableDepartments: iDepartment[]): Observable<iDepartment[]> {
+    //     return from(
+    //         fetch(this.apiEndpointUrl, {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify({
+    //                 departments: creatableDepartments
+    //             })
+    //         }).then(res => res.json())
+    //         .then(deps => deps)
+    //     )
+    // }
 }
